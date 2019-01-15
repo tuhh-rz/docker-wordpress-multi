cc7b69f9527f4d125bf43801a0a015debd53fcab
Andreas Böttger <andreas.boettger@tuhh.de>
2019-01-15 14:01:18 +0000

    Update 5.0/setup.sh, 4.9/setup.sh, 4.7/setup.sh files

--- 5.0/setup.sh
+++ 5.0/setup.sh
@@ -128,9 +128,6 @@ PHP
     su -s /bin/bash -c "/usr/local/bin/wp --path='/var/www/html/${RELATIVE_PATH}' plugin activate akismet --network" www-data
     # su -s /bin/bash -c "/usr/local/bin/wp --path='/var/www/html/${RELATIVE_PATH}' plugin activate stops-core-theme-and-plugin-updates --network" www-data
 
-    su -s /bin/bash -c "/usr/local/bin/wp --path='/var/www/html/${RELATIVE_PATH}' plugin deactivate stops-core-theme-and-plugin-updates --network" www-data
-    su -s /bin/bash -c "/usr/local/bin/wp --path='/var/www/html/${RELATIVE_PATH}' plugin delete stops-core-theme-and-plugin-updates" www-data
-
     su -s /bin/bash -c "/usr/local/bin/wp --path='/var/www/html/${RELATIVE_PATH}' plugin delete hello" www-data
     #su -s /bin/bash -c "/usr/local/bin/wp --path='/var/www/html/${RELATIVE_PATH}' plugin delete akismet" www-data
     
